659fa13d648458afea312eab05e1ace98ba188db
Paul Toraya <paul.toraya@gmail.com>
2024-04-04 23:13:03 -0700

    Se creo un nuevo archivo js y se conecto al html

---
+++ script.js
@@ -0,0 +1,4 @@
+// Este es un ejemplo de código JavaScript que simplemente muestra un mensaje de alerta al cargar la página.
+window.addEventListener('load', function() {
+    alert('¡Bienvenido a mi página!');
+});
